3eadda07e8b6bb05fd9428a23cabbaf0958e19da
Alex Goodman <alexthegoodman@gmail.com>
2023-12-07 12:39:09 -0500

    prompt tweaks

--- prompts/getFileList.ts
+++ prompts/getFileList.ts
@@ -1,5 +1,5 @@
 export const getDocumentList = (prompt) => `
-Provide a list of 5 document titles that are relevant to the prompt below:
+Provide a list of 5 document titles (like those kept as notes by consultants, startup founders, or researchers, rather than titles which sound like article titles) that are relevant to the prompt below:
 
 "${prompt}"
 
@@ -8,7 +8,7 @@ Answer with a JSON object containing an array of strings. For example:
 `;
 
 export const getAddtFilesList = (prompt) => `
-Provide a list of 3 presentation titles, 3 spreadsheet titles, and 3 image prompts that are relevant to the prompt below:
+Provide a list of 3 presentation titles, 3 spreadsheet titles, and 3 image prompts (like those kept as notes by consultants, startup founders, or researchers, rather than titles which sound like article titles) that are relevant to the prompt below:
 
 "${prompt}"
 
