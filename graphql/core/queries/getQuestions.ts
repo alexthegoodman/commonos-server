e7c84ca3e4a1a501cf6c775c9b0446b7ff58c545
Alex Goodman <alexthegoodman@gmail.com>
2023-12-06 19:02:35 -0500

    initial questions

---
+++ graphql/core/queries/getQuestions.ts
@@ -0,0 +1,68 @@
+import { extendType, nonNull, nullable, stringArg } from "nexus";
+import { Context } from "../../../context";
+import {
+  getFileQuestions,
+  getInitialQuestions,
+} from "../../../prompts/getQuestions";
+
+export const GetQuestionsQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("getQuestions", {
+      type: "JSON",
+      args: {
+        fileTitle: nonNull(stringArg()),
+        getThis: nonNull(stringArg()),
+      },
+      resolve: async (
+        _,
+        { flowId, fileTitle, getThis },
+        { prisma, openai, currentUser }: Context,
+        x
+      ) => {
+        console.info("getQuestions", fileTitle);
+
+        let content = "";
+        switch (getThis) {
+          case "initial":
+            content = getInitialQuestions(fileTitle);
+            break;
+          case "file":
+            content = getFileQuestions(fileTitle);
+            break;
+          default:
+            throw new Error("Invalid getThis");
+        }
+
+        // get continuation text from openai
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo-1106",
+          messages: [
+            {
+              content: content,
+              role: "user",
+            },
+          ],
+          response_format: { type: "json_object" },
+          // max_tokens: 50,
+          // temperature: 0.2,
+        });
+
+        console.info("openai response", response);
+
+        try {
+          const jsonText = response.choices[0].message.content;
+
+          if (!jsonText) {
+            throw new Error("No JSON text");
+          }
+
+          const json = JSON.parse(jsonText);
+          return json;
+        } catch (error) {
+          console.error("Error parsing OpenAI JSON", error);
+        }
+      },
+    });
+  },
+});
---
+++ prompts/getQuestions.ts
@@ -0,0 +1,17 @@
+export const getInitialQuestions = (prompt) => `
+Provide a list of 5 questions you have regarding the prompt below. Provide answers in multiple choice format, so it is easy for the user to pick an option. Ask questions that clarify the original prompt, rather than evaluate the user's knowledge of the prompt.
+
+"${prompt}"
+
+Answer with a JSON object containing an array of strings. For example:
+{"questions":[{"question":"Question 1","answers":["Answer 1","Answer 2","Answer 3"]},{"question":"Question 2","answers":["Answer 1","Answer 2","Answer 3"]}]}
+`;
+
+export const getFileQuestions = (prompt) => `
+Provide a list of 5 questions you have regarding the file (title) below. Provide answers in multiple choice format, so it is easy for the user to pick an option. Ask questions that clarify what the user wants in the file, rather than evaluate the user's knowledge.
+
+"${prompt}"
+
+Answer with a JSON object containing an array of strings. For example:
+{"questions":[{"question":"Question 1","answers":["Answer 1","Answer 2","Answer 3"]},{"question":"Question 2","answers":["Answer 1","Answer 2","Answer 3"]}]}
+`;
