a7356712f0479a762f1301d1b25273f01e3a1a20
Alex Goodman <alexthegoodman@gmail.com>
2023-12-23 19:01:12 -0500

    fixes

--- prompts/getGuideQuestions.ts
+++ prompts/getGuideQuestions.ts
@@ -12,6 +12,7 @@ Provide a list of 3 multiple choice questions (each with 4 selections).
 Considering the goal of the title, ask in-depth questions regarding what the user wants in the sections provided. 
 Make sure the questions extract information that is not already provided in the sections.
 Avoid asking about what the user likes, what appeals to them, or what they are interested in, instead ask about what they want in the sections.
+Also avoid asking the questions which are already answered by the sections.
 
 Answer with a JSON object containing an array of strings. For example:
 {"questions":[{"question":"Question 1","answers":["Answer 1","Answer 2","Answer 3"]},{"question":"Question 2","answers":["Answer 1","Answer 2","Answer 3"]}]}
@@ -31,6 +32,7 @@ Provide a list of 3 multiple choice questions (each with 4 selections).
 Considering the goal of the title, ask in-depth questions regarding what the user wants in the sections provided. 
 Make sure the questions extract information that is not already provided in the sections.
 Avoid asking about what the user likes, what appeals to them, or what they are interested in, instead ask about what they want in the sections.
+Also avoid asking the questions which are already answered by the sections.
 
 Answer with a JSON object containing an array of strings. For example:
 {"questions":[{"question":"Question 1","answers":["Answer 1","Answer 2","Answer 3"]},{"question":"Question 2","answers":["Answer 1","Answer 2","Answer 3"]}]}
